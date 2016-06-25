086c887157a8bbf06fa62f21662fcc90c7da00ef
licl <476020050@qq.com>
2016-06-25 23:50:14 +0800

    预添加了功能项2一级页面的滑动动画

--- app/src/main/java/com/example/pc/vita/Fragment/YuePaiFragment.java
+++ app/src/main/java/com/example/pc/vita/Fragment/YuePaiFragment.java
@@ -18,44 +18,15 @@ import java.util.List;
 
 public class YuePaiFragment extends Fragment {
 
-    private Button button_YuePai;
-    private Button button_Activity;
-
-    private ListView activityList;
-    private ActivityAdapter adapter;
-
-    private List<ActivityBrief> myList;
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_yue_pai, container, false);
 
-        FragmentInit(view);
-
         return view;
     }
 
-    private boolean FragmentInit(View view){
-        myList= new ArrayList<>();
 
-        cleanList();
-        readList();
 
-        adapter = new ActivityAdapter(YuePaiFragment.this.getContext(),R.layout.activity_item_view,myList);
-
-        activityList = (ListView)view.findViewById(R.id.activty_list);
-        activityList.setAdapter(adapter);
-
-        return true;
-    }
-
-    private void cleanList(){
-        myList.clear();
-    }
-
-    //从服务器中读取活动列表
-    private void readList(){
-    }
 }
