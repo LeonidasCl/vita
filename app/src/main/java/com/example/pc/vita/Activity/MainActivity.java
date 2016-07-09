ebb13977c6d5dc3680845892387e053216a7f848
licl <476020050@qq.com>
2016-07-09 22:56:18 +0800

    加了一点点微小的边框

--- app/src/main/java/com/example/pc/vita/Activity/MainActivity.java
+++ app/src/main/java/com/example/pc/vita/Activity/MainActivity.java
@@ -315,29 +315,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }*/
 
-    @Override
-    public boolean onMenuOpened(int featureId,Menu menu){
-
-        boolean ret= super.onMenuOpened(featureId,menu);
-        setOverflowIconVisible(featureId,menu);
-        return ret;
-    }
-    /**
-     * 利用反射让隐藏在Overflow中的MenuItem显示Icon图标
-     * @param featureId
-     * @param menu
-     * onMenuOpened方法中调用
-     */
-    public static void setOverflowIconVisible(int featureId, Menu menu) {
-        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
-            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
-                try {
-                    Method m = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
-                    m.setAccessible(true);
-                    m.invoke(menu, true);
-                } catch (Exception e) {
-                }
-            }
-        }
-    }
 }
