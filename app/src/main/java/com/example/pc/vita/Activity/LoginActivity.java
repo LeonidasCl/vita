d881360fa341840be1ceb8096b0097afdab617b4
licl <476020050@qq.com>
2016-07-20 20:47:43 +0800

    准备客户端验证码接口

--- app/src/main/java/com/example/pc/vita/Activity/LoginActivity.java
+++ app/src/main/java/com/example/pc/vita/Activity/LoginActivity.java
@@ -3,6 +3,7 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -38,10 +39,12 @@ public class LoginActivity extends AppCompatActivity implements PhotoCallbackInt
     private TextView signup;
     private TextView verifycode;
     private Button btn_login;
+    private Button btn_verifycode;
     private NetRequest requestFragment;
     private ProgressDialog loginProgressDlg;
     private int loginReturn;
     private ImageView loginbtnimg;
+    private CountDownTimer timeCount;
     private int eventFlag=1;//1为登录 2为忘记密码 3为注册
     TranslateAnimation animationHide=new TranslateAnimation(Animation.RELATIVE_TO_SELF,
             0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
@@ -63,6 +66,13 @@ public class LoginActivity extends AppCompatActivity implements PhotoCallbackInt
             }
         }
     };
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        finish();
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -95,6 +105,13 @@ public class LoginActivity extends AppCompatActivity implements PhotoCallbackInt
                 }
             }
         });
+        btn_verifycode=(Button)findViewById(R.id.btn_verify_code);
+        btn_verifycode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeCount.start();
+            }
+        });
         requestFragment=new NetRequest(this,this);
         loginbtnimg=(ImageView)findViewById(R.id.loginimgview);
         loginbtnimg.bringToFront();
@@ -103,6 +120,7 @@ public class LoginActivity extends AppCompatActivity implements PhotoCallbackInt
             @Override
             public void onClick(View v) {
                 if(eventFlag==2){
+                    signup.setVisibility(View.VISIBLE);
                     eventFlag=1;
                     forgotpassword.setText("忘记密码了");
                     animationHide.setDuration(500);
@@ -110,10 +128,12 @@ public class LoginActivity extends AppCompatActivity implements PhotoCallbackInt
                     verifycode.setVisibility(View.GONE);
                     animationShow.setDuration(500);
                     password.startAnimation(animationShow);
+                    btn_verifycode.setVisibility(View.GONE);
                     password.setVisibility(View.VISIBLE);
                     return;
                 }
                 if(eventFlag!=2){
+                signup.setVisibility(View.INVISIBLE);
                 eventFlag=2;
                 animationHide.setDuration(500);
                 password.startAnimation(animationHide);
@@ -121,6 +141,7 @@ public class LoginActivity extends AppCompatActivity implements PhotoCallbackInt
                 animationShow.setDuration(500);
                 verifycode.startAnimation(animationShow);
                 verifycode.setVisibility(View.VISIBLE);
+                btn_verifycode.setVisibility(View.VISIBLE);
                 forgotpassword.setText("找回了密码");}
                 return;
             }
@@ -138,6 +159,8 @@ public class LoginActivity extends AppCompatActivity implements PhotoCallbackInt
                 verifycode.startAnimation(animationShow);
                 verifycode.setVisibility(View.VISIBLE);
                 signup.setText("老用户登录");
+                    forgotpassword.setVisibility(View.INVISIBLE);
+                    btn_verifycode.setVisibility(View.VISIBLE);
                     btn_login.setText("  注   册");
                     return;
                 }
@@ -150,11 +173,30 @@ public class LoginActivity extends AppCompatActivity implements PhotoCallbackInt
                     password.startAnimation(animationShow);
                     password.setVisibility(View.VISIBLE);
                     signup.setText("新用户注册");
+                    btn_verifycode.setVisibility(View.GONE);
+                    forgotpassword.setVisibility(View.VISIBLE);
                     btn_login.setText("  登   录");
                     return;
                 }
             }
         });
+        timeCount=new CountDownTimer(60000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //btn_verifycode.setBackgroundColor(getResources().getColor(R.drawable.shape_verifycode));
+                btn_verifycode.setBackground(getResources().getDrawable(R.drawable.shape_verifycode_clicked));
+                btn_verifycode.setClickable(false);
+                btn_verifycode.setText("重发 "+millisUntilFinished/1000+"s");
+            }
+
+            @Override
+            public void onFinish() {
+                btn_verifycode.setText(R.string.text_verify_code);
+                //btn_verifycode.setBackgroundColor(getResources().getColor(R.color.gold));
+                btn_verifycode.setBackground(getResources().getDrawable(R.drawable.shape_verifycode));
+                btn_verifycode.setClickable(true);
+            }
+        };
 
         Intent intent = getIntent();
         String value = intent.getStringExtra("method");
